73a482f5234b8b73856403b4db076bae537f9051
mari <mari.17ymy@gmail.com>
2020-07-20 00:26:35 -0400

    descripcion del zapato

--- lib/src/pages/zapato_page.dart
+++ lib/src/pages/zapato_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:shoes_app/src/widgets/custom_appbar_widget.dart';
+import 'package:shoes_app/src/widgets/descrip_zapato_widget.dart';
 import 'package:shoes_app/src/widgets/zapatoSizePreview.dart';
 
 class ZapatoPage extends StatelessWidget {
@@ -12,7 +13,19 @@ class ZapatoPage extends StatelessWidget {
         children: [
           CustomAppBarWidget('for you'),
           SizedBox(height: 20.0),
-          ZapatoSizePreviewWidget()
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  ZapatoSizePreviewWidget(),
+                  ZapatoDescripcionWidget(
+                    titulo: 'Nike Air Max 720',
+                    descripcion: "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
+                  )
+                ],
+              ),
+            )
+          ),  
         ],
       )
     );
---
+++ lib/src/widgets/descrip_zapato_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ZapatoDescripcionWidget extends StatelessWidget {
+  final String titulo;
+  final String descripcion;
+  
+  ZapatoDescripcionWidget({ @required this.titulo, @required this.descripcion});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal:30.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 20.0,),
+          Text(this.titulo, style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w700),),
+          SizedBox(height: 20.0,),
+          Text(this.descripcion, style: TextStyle(color: Colors.black54, height: 1.6,))
+        ],
+      ),
+    );
+  }
+}
