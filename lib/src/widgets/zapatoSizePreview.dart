bf29af5a40c386d07307d248188adcb0b8e08db7
mari <mari.17ymy@gmail.com>
2020-07-19 23:08:06 -0400

    sombra del zapato

--- lib/src/widgets/zapatoSizePreview.dart
+++ lib/src/widgets/zapatoSizePreview.dart
@@ -8,7 +8,7 @@ class ZapatoSizePreviewWidget extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 5.0),
       child: Container(
         width: double.infinity,
-        height: 300.0,
+        height: 400.0,
         decoration: BoxDecoration(
           color: Color(0xffF8D468),
           borderRadius: BorderRadius.circular(50.0)
@@ -33,8 +33,29 @@ class ZapatoConSombra extends StatelessWidget {
       padding: EdgeInsets.all(50.0),
       child: Stack(
         children: [
-          Image(image: AssetImage('assets/imgs/azul.png'))
+          Image(image: AssetImage('assets/imgs/azul.png')),
+          _Sombra()
         ],
+      ),
+    );
+  }
+}
+
+class _Sombra extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: -0.5,
+      child: Container(
+        height: 100.0, width: 200.0,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(100.0),
+          boxShadow: <BoxShadow>[
+            BoxShadow( color: Color(0xffEAA14E), blurRadius: 40.0)
+          ]
+        ),
       ),
     );
   }
