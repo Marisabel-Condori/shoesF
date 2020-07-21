7d686a022eb9f75660c7ede74b381e6175c009a4
mari <mari.17ymy@gmail.com>
2020-07-20 20:38:30 -0400

    iconos y botones

--- lib/src/pages/pantalla_descripcion_page.dart
+++ lib/src/pages/pantalla_descripcion_page.dart
@@ -17,7 +17,8 @@ class PantallaDescripcionPage extends StatelessWidget {
               descripcion: "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",        
             ),
             _MontoByNow(),
-            _ColoresYmas()
+            _ColoresYmas(),
+            _BotonesCardSettings()
           ],
         ),
       ),
@@ -25,8 +26,51 @@ class PantallaDescripcionPage extends StatelessWidget {
   }
 }
 
-class _MontoByNow extends StatelessWidget {
+class _BotonesCardSettings extends StatelessWidget {
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 30.0),
+      padding: EdgeInsets.symmetric(horizontal: 30.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _CardPersonalizado(Icon(Icons.favorite, color: Colors.red,), ),
+          _CardPersonalizado(Icon(Icons.star, color: Colors.grey.withOpacity(0.4),),),
+          _CardPersonalizado(Icon(Icons.settings, color: Colors.grey.withOpacity(0.4),),)
+        ],
+      ),
+    );
+  }
+}
+
+class _CardPersonalizado extends StatelessWidget {
+  final Icon iconB;
+
+  _CardPersonalizado(this.iconB);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 40.0, height: 40.0,
+      child: iconB,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black38, 
+            spreadRadius: -5, 
+            blurRadius: 20, 
+            offset: Offset(0, 10)
+          )
+        ]
+      ),
+    );
+  }
+}
+
+class _MontoByNow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
