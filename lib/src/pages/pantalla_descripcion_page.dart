266320bf2b52769fc0f58e94208f9eda6d2b53a0
mari <mari.17ymy@gmail.com>
2020-07-20 20:47:57 -0400

    flecha

--- lib/src/pages/pantalla_descripcion_page.dart
+++ lib/src/pages/pantalla_descripcion_page.dart
@@ -11,7 +11,20 @@ class PantallaDescripcionPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            ZapatoSizePreviewWidget( screenSize: true,),
+            Stack(
+              children: [
+                ZapatoSizePreviewWidget( screenSize: true,),
+                Positioned(
+                  top: 70.0,
+                  child: FloatingActionButton(
+                    child: Icon(Icons.chevron_left, color: Colors.white, size: 60,),
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    highlightElevation: 0,
+                    onPressed: (){}),
+                )
+              ],
+            ),
             ZapatoDescripcionWidget(
               titulo: 'Nike Air Max 720',
               descripcion: "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",        
